4eaf6fe7b23e036c9a60eb9c8ba6a94cb29031e7
archercreat <archercreat@gmail.com>
2018-10-30 19:05:43 +0300

    added functionality

--- task_queue/server.py
+++ task_queue/server.py
@@ -1,47 +1,61 @@
 
 import argparse
+import socket
 
 class TaskQueueServer:
 
-    def __init__(self, ip, port, path, timeout):
-        pass
+	def __init__(self, ip, port, path, timeout):
+		self.ip = ip
+		self.port = port
+		self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    def run(self):
-        pass
+	def run(self):
+		self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+		self.s.bind((self.ip, self.port))
+		self.s.listen(10)
+		self.__main()
+
+	def close(self):
+		self.s.close()
+
+	def __main(self):
+		while True:
+			conn, addr = self.s.accept()
 
 def parse_args():
-    parser = argparse.ArgumentParser(description='This is a simple task queue server with custom protocol')
-    parser.add_argument(
-        '-p',
-        action="store",
-        dest="port",
-        type=int,
-        default=5555,
-        help='Server port')
-    parser.add_argument(
-        '-i',
-        action="store",
-        dest="ip",
-        type=str,
-        default='0.0.0.0',
-        help='Server ip adress')
-    parser.add_argument(
-        '-c',
-        action="store",
-        dest="path",
-        type=str,
-        default='./',
-        help='Server checkpoints dir')
-    parser.add_argument(
-        '-t',
-        action="store",
-        dest="timeout",
-        type=int,
-        default=300,
-        help='Task maximum GET timeout in seconds')
-    return parser.parse_args()
+	parser = argparse.ArgumentParser(description='This is a simple task queue server with custom protocol')
+	parser.add_argument(
+		'-p',
+		action="store",
+		dest="port",
+		type=int,
+		default=5555,
+		help='Server port')
+	parser.add_argument(
+		'-i',
+		action="store",
+		dest="ip",
+		type=str,
+		default='0.0.0.0',
+		help='Server ip adress')
+	parser.add_argument(
+		'-c',
+		action="store",
+		dest="path",
+		type=str,
+		default='./',
+		help='Server checkpoints dir')
+	parser.add_argument(
+		'-t',
+		action="store",
+		dest="timeout",
+		type=int,
+		default=300,
+		help='Task maximum GET timeout in seconds')
+	return parser.parse_args()
 
 if __name__ == '__main__':
-    args = parse_args()
-    server = TaskQueueServer(**args.__dict__)
-    server.run()
+	args = parse_args()
+	print(args)
+	server = TaskQueueServer(**args.__dict__)
+	server.run()
