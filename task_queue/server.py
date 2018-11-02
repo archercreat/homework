a54fbe6b4baa13f827888085b6734f447aed4972
archercreat <archercreat@gmail.com>
2018-11-02 15:12:50 +0300

    new

--- task_queue/server.py
+++ task_queue/server.py
@@ -3,10 +3,35 @@ import argparse
 import socket
 import time
 import json
+from datetime import datetime
 import sys
+import os
+import pickle
+from collections import OrderedDict
+from uuid import uuid1
+
+
+class Task:
+	def __init__(self, length, data):
+		self.length = length
+		self.data = data
+		self.id = uuid1().hex
+		self.time = 0
+
+	def __repr__(self):
+		return f'id ({self.id}), time ({self.time})'
+
+	def is_in_work(self, current_time, timeout):
+		return current_time - self.time < timeout
+
+	def set_time(self):
+		self.time = int(datetime.now().timestamp())
+
+
 
 
 class TaskQueueServer:
+	filename = 'log'
 
 	def __init__(self, ip, port, path, timeout):
 		self.ip = ip
@@ -15,14 +40,28 @@ class TaskQueueServer:
 		self.timeout = timeout
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.buffer = 4096
+		self.filepath = os.path.join(self.path, self.filename)
+		self.commands = {
+			'ADD':	self.add_command,
+			'GET':	self.get_command,
+			'ACK':	self.ack_command,
+			'IN':	self.in_command,
+			'SAVE':	self.save_command,
+		}
+		self.current_time = 0
+		self.LIST_OF_QUEUES = self.load()
+
+
+	def load(self):
+		return {}
 
 
 	def recvall(self, conn):
 		data = b''
 		while True:
-			buff = conn.recv(self.buffer)
-			data += buff
-			if len(buff) < self.buffer:
+			buf = conn.recv(self.buffer)
+			data += buf
+			if len(buf) < self.buffer:
 				break
 		return data
 
@@ -35,13 +74,15 @@ class TaskQueueServer:
 		try:
 			self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 			self.sock.bind((self.ip, self.port))
-			print(f'server is running on {self.ip} on port {self.port}')
 			self.sock.listen()
+			print(f'server is running on {self.ip} on port {self.port}')
 			self.main()
+
 		except KeyboardInterrupt:
-			print('\nclosing server')
+			print('\nclosing server..')
 			self.sock.close()
 			sys.exit(0)
+
 
 	def main(self):
 		while True:
@@ -49,21 +90,73 @@ class TaskQueueServer:
 			data = self.recvall(conn).decode().rstrip().split()
 			if data[0] == 'SAVE':
 				conn.send(self.save().encode())
+			else:
+				handle = ConnectionHandler(conn)
+				handle.operate()
 			conn.close()
 
-
+'''
 class ConnectionHandler:
 	def __init__(self, conn):
 		self.conn = conn
 		self.commands = Commands()
+		# self.LIST_OF_QUEUES
 
 
-	def work(self):
+	def operate(self):
 		# print(f'You are here! {self.conn}')
 		# waiting for message
 		recv = self.conn.recv(1024).split()
 		if self.commands.parse(recv) is None:
 			self.conn.close()
+
+
+	def add_cmd(self, user_input):
+		in not user_input or len(user_input) != 3:
+			return
+		try:
+			str(queue_name), int(length), str(data) = user_input
+		except ValueError:
+			return
+		if length > 10**6 or length != len(data):
+			return
+
+
+	def in_cmd(self, user_input):
+		if not user_input or len(user_input) != 2:
+			return
+		queue_name, task_id = user_input
+		queue = self.LIST_OF_QUEUES.get(queue_name)
+		if not queue:
+			return
+
+		for task in queue['tasks']:
+			if task['id'] == task_id:
+				return b'YES'
+		return b'NO'
+
+	def get_cmd(self, user_input):
+		if not user_input or len(user_input) != 1:
+			return
+		queue_name = user_input
+		queue = self.LIST_OF_QUEUES.get(queue_name)
+		if not queue:
+			return 'NONE'
+		return queue		
+
+	def ack_cmd(self, user_input):
+		if not user_input or len(user_input) != 2:
+			return
+		queue_name, task_id = user_input
+		queue = self.LIST_OF_QUEUES.get(queue_name)
+		if not queue:
+			return
+		for task in queue['tasks']:
+			if task['id'] == task_id:
+				return 'YES'
+		return 'NO'
+'''
+
 
 
 def parse_args():
