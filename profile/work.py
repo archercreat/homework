756f2bcdcd9f5c9528774fce6c240a8053fb24ea
archercreat <archercreat@gmail.com>
2018-11-03 23:55:50 +0300

    second try

--- profile/work.py
+++ profile/work.py
@@ -1,34 +1,63 @@
 from functools import wraps
+from datetime import datetime
 import time
 
-
 def profile(func):
-	for attr_name in func.__dict__:
-		# print(getattr(func, attr_name))
-		pass
 	@wraps(func)
 	def new_func(*args, **kwargs):
 		name = new_func.__name__
 		print(f'`{name}` started')
-		start = time.time()
+		start = datetime.now().timestamp()
 		result =  func(*args, **kwargs)
-		stop = time.time()
-		print('`%s` finished in %2.2f sec' % (new_func.__name__, stop - start))
+		stop = datetime.now().timestamp()
+		print('`%s` finished in %f sec' % (new_func.__name__, stop - start))
 		return result
-
 	return new_func
 
-@profile
+def time_all_class_methods(Cls):
+	class NewCls:
+		def __init__(self,*args,**kwargs):
+			self.oInstance = Cls(*args,**kwargs)
+		def __getattribute__(self,s):
+			try:
+				x = super(NewCls,self).__getattribute__(s)
+			except AttributeError:
+				pass
+			else:
+				return x
+			x = self.oInstance.__getattribute__(s)
+			if type(x) == type(self.__init__):
+				return time_this(x)
+			else:
+				return x
+
+	def time_this(original_function):
+		def new_function(*args,**kwargs):
+			from datetime import datetime
+			before = datetime.now()
+			result = original_function(*args,**kwargs)
+			after = datetime.now()
+			print("Elapsed Time = {0}".format(after-before))
+			return result
+		return new_function
+	return NewCls
+
+
+
+@time_all_class_methods
 def foo(x, y):
 	return x + y
 
-@profile
+@time_all_class_methods
 class Bar:
 	def __init__(self):
 		self.name = 'Bob'
 		self.age  = '20'
 
-	def __call__(self):
-		return self.name
+	def rofl(self):
+		time.sleep(3)
+		return 'LOL'
 
-foo(1, 2)
+oF = Bar()
+print(oF.rofl())
+print(foo(1, 2))
