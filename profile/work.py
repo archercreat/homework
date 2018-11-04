505f75eba1fa4729b8cb7c5476182c1ddceea3fe
archercreat <archercreat@gmail.com>
2018-11-04 16:41:37 +0300

    done

--- profile/work.py
+++ profile/work.py
@@ -1,63 +1,50 @@
-from functools import wraps
-from datetime import datetime
-import time
+def profile(obj):
+	from functools import wraps
+	from datetime import datetime
+	import inspect
 
-def profile(func):
-	@wraps(func)
-	def new_func(*args, **kwargs):
-		name = new_func.__name__
-		print(f'`{name}` started')
-		start = datetime.now().timestamp()
-		result =  func(*args, **kwargs)
-		stop = datetime.now().timestamp()
-		print('`%s` finished in %f sec' % (new_func.__name__, stop - start))
-		return result
-	return new_func
-
-def time_all_class_methods(Cls):
-	class NewCls:
-		def __init__(self,*args,**kwargs):
-			self.oInstance = Cls(*args,**kwargs)
-		def __getattribute__(self,s):
-			try:
-				x = super(NewCls,self).__getattribute__(s)
-			except AttributeError:
-				pass
-			else:
-				return x
-			x = self.oInstance.__getattribute__(s)
-			if type(x) == type(self.__init__):
-				return time_this(x)
-			else:
-				return x
-
-	def time_this(original_function):
-		def new_function(*args,**kwargs):
-			from datetime import datetime
-			before = datetime.now()
-			result = original_function(*args,**kwargs)
-			after = datetime.now()
-			print("Elapsed Time = {0}".format(after-before))
+	def func_wrapper(func):
+		@wraps(func)
+		def new_func(*args, **kwargs):
+			print(f'`{func.__name__}` started')
+			start = datetime.now().timestamp()
+			result = func(*args, **kwargs)
+			stop = datetime.now().timestamp()
+			print('`%s` finished in %f'% (func.__name__, stop - start))
 			return result
-		return new_function
-	return NewCls
+		return new_func
+
+	def class_wrapper(klass):
+		class_objs = [(name, obj) for name, obj in klass.__dict__.items() if inspect.isfunction(obj)]
+		for class_obj in class_objs:
+			name, _ = class_obj
+			attr = getattr(klass, name)
+			setattr(klass, name, func_wrapper(attr))
+		return klass
+
+	if inspect.isfunction(obj):
+		return func_wrapper(obj)
+	elif inspect.isclass(obj):
+		return class_wrapper(obj)
 
 
-
-@time_all_class_methods
+@profile
 def foo(x, y):
 	return x + y
 
-@time_all_class_methods
+@profile
 class Bar:
 	def __init__(self):
 		self.name = 'Bob'
 		self.age  = '20'
 
 	def rofl(self):
-		time.sleep(3)
 		return 'LOL'
+
+	def mom(self):
+		return 'KEK'
 
 oF = Bar()
 print(oF.rofl())
+print(oF.mom())
 print(foo(1, 2))
