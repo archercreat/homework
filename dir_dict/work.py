66ef848df7c163be294e72237cbc561dc80ab390
archercreat <archercreat@gmail.com>
2018-11-04 22:45:24 +0300

    done

--- dir_dict/work.py
+++ dir_dict/work.py
@@ -1,47 +1,40 @@
 import os
-from collections import MutableMapping, Set
+from collections import MutableMapping
 
 class DictView(MutableMapping):
-	def __init__(self, directory='./'):
-		self.dir = directory
-		self.listdir = os.listdir(self.dir)
+	def __init__(self, **kwargs):
+		self.dir = './'
+		[self.__setitem__(key, value) for key, value in kwargs.items() if kwargs]
 
 	def __call__(self):
-		return {}
+		return os.listdir(self.dir)
 
 	def __len__(self):
-		return len(self.listdir)
+		return len(os.listdir(self.dir))
 
 	def __reversed__(self):
-		return iter(self.listdir[::-1])
+		return iter(os.listdir(self.dir)[::-1])
 
 	def __contains__(self, key):
-		return True if key in self.listdir else False
+		return True if key in os.listdir(self.dir) else False
 
 	def __iter__(self):
-		return iter(self.listdir)
+		return iter(os.listdir(self.dir))
 
 	def __repr__(self):
-		return f'{self.listdir}'
+		return f'{os.listdir(self.dir)}'
 
 	def __getitem__(self, key):
-		if key not in self.listdir:
+		if key not in os.listdir(self.dir):
 			raise KeyError(key)
 		with open(os.path.join(self.dir, key), 'r', encoding='utf8') as f:
 			return f.read()
 
-	def __setitem__(self, key):
-		pass
-
+	def __setitem__(self, key, value):
+		with open(os.path.join(self.dir, key), 'w', encoding='utf8') as f:
+			f.write(value)
 
 	def __delitem__(self, key):
-		if key not in self.listdir:
+		if key not in os.listdir(self.dir):
 			raise KeyError(key)
 		os.remove(key)
-
-
-x = DictView()
-if __name__ == "__main__":
-	print(len(x))
-	print(x)
-	# del x['y']
