796bf60dc87daf880038b17941b9168b391ec43d
ArcherCreat <archercreat@gmail.com>
2018-11-14 23:50:53 +0300

    .

--- whenthen/work.py
+++ whenthen/work.py
@@ -1,21 +1,51 @@
 from functools import wraps
 
 class whenthen:
-    def __init__(self):
-        self.order = True
+    def __init__(self, func):
+        self._when = False
+        self.func = func
+
+    def __call__(self, *args, **kwags):
+        pass
 
     def when(self, func):
-        if self.order:
-            pass
+        if not self._when:
+            # code
+            self._when = True
         else:
             raise AttributeError("then is needed")
 
     def then(self, func):
-        pass
+        # code
+        self._when = False
 
 
 def main():
-    pass
+    @whenthen
+    def fract(x):
+        return x * fract(x - 1)
+
+
+    @fract.when
+    def fract(x):
+        return x == 0
+
+
+    @fract.then
+    def fract(x):
+        return 1
+
+
+    @fract.when
+    def fract(x):
+        return x > 5
+
+
+    @fract.then
+    def fract(x):
+        return x * (x - 1) * (x - 2) * (x - 3) * (x - 4) * fract(x - 5)
+
+    print(fract(0))
 
 if __name__ == '__main__':
     main()
