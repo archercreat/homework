30b944bb44f278bab9744910788139cd7579564c
ArcherCreat <archercreat@gmail.com>
2018-11-14 23:15:39 +0300

    added class whenthen

--- whenthen/work.py
+++ whenthen/work.py
@@ -1,3 +1,19 @@
+from functools import wraps
+
+class whenthen:
+    def __init__(self):
+        self.order = True
+
+    def when(self, func):
+        if self.order:
+            pass
+        else:
+            raise AttributeError("then is needed")
+
+    def then(self, func):
+        pass
+
+
 def main():
     pass
 
